7b2a2aa039f25a1532d82ed7f67038bdd7945e35
sfunk1x <sfunk1x@users.noreply.github.com>
2024-05-03 14:20:35 -0700

    Update nextcloud-update.sh
    Cleanup and refining. Still focused on nginx, but future changes should support whatever web server host is being used.

--- nextcloud-update.sh
+++ nextcloud-update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # this script needs to be run as ROOT (sudo) in order for these actions to be performed properly
 
+NEXTCLOUD_OLD_INSTALL_FOLDER=`ls -d /usr/share/nginx/html/nextcloud-* | xargs -n 1 basename`
+NEXTCLOUD_OLD_INSTALL_LOCATION="/usr/share/nginx/html/${NEXTCLOUD_OLD_INSTALL_FOLDER}"
 NEXTCLOUD_ARCHIVE_URL_ROOT="https://download.nextcloud.com/server/releases"
 NEXTCLOUD_ARCHIVE_FILENAME_ROOT="nextcloud"
 NEXTCLOUD_ARCHIVE_EXTENSION="zip"
 NEXTCLOUD_DESTINATION_FOLDER_PARENT="/usr/share/nginx/html"
+NGINX_USER="nginx"
+NGINX_GROUP="nginx"
 SHOULD_BE_ROOT="root"
 
 # user must be root to do all the things. check and exit if not root.
@@ -39,9 +43,9 @@ extract_and_move_nextcloud_archive()
   NEXTCLOUD_DESTINATION_FOLDER="nextcloud-${VERSION_TO_DOWNLOAD}"
   echo "Extracting nextcloud/ from archive and moving to $NEXTCLOUD_DESTINATION_FOLDER ..."
   cd /tmp
-  echo "unzipping $NEXTCLOUD_ARCHIVE_FILENAME" 
+  echo "Unzipping $NEXTCLOUD_ARCHIVE_FILENAME ..." 
   unzip -qq $NEXTCLOUD_ARCHIVE_FILENAME
-  echo "moving extracted nextcloud/ folder to $NEXTCLOUD_DESTINATION_FOLDER"
+  echo "Moving extracted nextcloud/ folder to $NEXTCLOUD_DESTINATION_FOLDER ..."
   mv nextcloud/ $NEXTCLOUD_DESTINATION_FOLDER
   rm -rf /tmp/$NEXTCLOUD_ARCHIVE_FILENAME
 }
@@ -63,7 +67,7 @@ copy_old_nextcloud_configuration()
 setup_nextcloud_symlink()
 {
   cd $NEXTCLOUD_DESTINATION_FOLDER_PARENT
-  echo "removing nextcloud symlink from previous version of nextcloud"
+  echo "Removing nextcloud symlink from previous version of nextcloud ..."
   unlink nextcloud
   echo "Creating nextcloud symlink to $NEXTCLOUD_DESTINATION_FOLDER ..."
   ln -s $NEXTCLOUD_DESTINATION_FOLDER nextcloud
@@ -71,24 +75,24 @@ setup_nextcloud_symlink()
 
 fix_ownership_permissions_of_nextcloud()
 {
-  echo "changing folder to $NEXTCLOUD_DESTINATION_FOLDER"
+  echo "Changing folder to $NEXTCLOUD_DESTINATION_FOLDER ..."
   cd $NEXTCLOUD_DESTINATION_FOLDER_PARENT
   echo "Fixing ownership and permissions of $NEXTCLOUD_DESTINATION_FOLDER ..."
-  sudo chown -R nginx:nginx $NEXTCLOUD_DESTINATION_FOLDER
+  sudo chown -R $NGINX_USER:$NGINX_GROUP $NEXTCLOUD_DESTINATION_FOLDER
   find $NEXTCLOUD_DESTINATION_FOLDER -type d -exec chmod 750 {} \;
   find $NEXTCLOUD_DESTINATION_FOLDER -type f -exec chmod 640 {} \;
 }
 
 setup_new_nextcloud_version()
 {
-  echo "changing folder to $NEXTCLOUD_DESTINATION_FOLDER_PARENT/nextcloud"
+  echo "Changing folder to $NEXTCLOUD_DESTINATION_FOLDER_PARENT/nextcloud ..."
   cd $NEXTCLOUD_DESTINATION_FOLDER_PARENT/nextcloud
   echo "Turning on maintenance mode ..."
-  sudo -u nginx php occ maintenance:mode --on
+  sudo -u $NGINX_USER php occ maintenance:mode --on
   echo "Executing occ upgrade ..."
-  sudo -u nginx php occ upgrade
+  sudo -u $NGINX_USER php occ upgrade
   echo "Turning off maintenance mode ..."
-  sudo -u nginx php occ maintenance:mode --off
+  sudo -u $NGINX_USER php occ maintenance:mode --off
 }
 
 
@@ -98,7 +102,12 @@ restart_services()
   systemctl restart php8.2-fpm nginx
 }
 
-# Sleeps were added for debugging - they can be removed if you care to remove them.
+remove_old_nextcloud_folder()
+{
+  echo "Removing old Nextcloud installation folder - $NEXTCLOUD_OLD_INSTALL_LOCATION ..."
+  rm -rf $NEXTCLOUD_OLD_INSTALL_LOCATION
+}
+
 construct_nextcloud_archive_url
 sleep 5
 download_nextcloud_archive
@@ -113,3 +122,4 @@ setup_nextcloud_symlink
 fix_ownership_permissions_of_nextcloud
 setup_new_nextcloud_version
 restart_services
+remove_old_nextcloud_folder
